14f61fdd7c7e7191ab64e77cc855e1e5ef42f73f
tjuant1 <juanpatrickpds@gmail.com>
2024-08-22 22:26:15 -0300

    small fixes

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,14 +1,7 @@
-import sys
-import os
-
-# Add current directory to path
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 from robot import run_cli
 
-if __name__ == "__main__":
-    # Define current directory
-
-    # Run tests and save results in results directory
-    result = run_cli(["-d", "../results", "." ])
-    sys.exit(result)
+# Run tests and save results in results directory
+run_cli ([
+    "-d", "../logs", 
+    "." 
+])
